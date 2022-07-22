a306c627d6fb12df1c9d6be3f42cfa5fcfedfd75
ashutoshbhakare <bhakareashutosh@yahoo.com>
2022-07-22 22:46:24 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world FROM WEBHOOKS v222 This is Pavan and ASHUTOSH !! Hi and Bye  hello is the string needed here.";
+    return "Hello WORLD This is Pavan and ASHUTOSH !! Hi and Bye  hello is the string needed here.";
   }
 }
