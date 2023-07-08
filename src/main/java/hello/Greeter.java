692689ec0025cde09c6a9cd63b4ac5e532a451b6
ashutoshbhakare <bhakareashutosh@yahoo.com>
2023-07-08 21:46:54 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello WORLD HI FROM KUCL 2.0000000 !! Hi and Bye  hello is the string needed here. testig for CKACLASS for webhooks";
+    return "Hello WORLD welcome to unnati !! Hi and Bye  hello is the string needed here. testig for CKACLASS for webhooks";
   }
 }
