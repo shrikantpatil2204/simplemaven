e320cc452ace590225891d3b5a9d0153fbe0f67b
MaheshChauthe <102281195+Mahesh-Chauthe@users.noreply.github.com>
2023-07-13 18:56:40 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Welcome to Unnati Development....." !!!! Bye";
+    return "Welcome to unnati development and training centre" !!!! Bye";
   }
 }
